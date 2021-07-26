5baee94480cf37b6c1accd7098a689d00a818748
QuocKhanh2k1 <trannguyenquockhanh0809@gmail.com>
2021-07-26 12:45:25 +0700

    Add ReactJS folder

---
+++ react-hooks/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter } from "react-router-dom";
+import configureProductsStore from "./hooks-store/products-store";
+
+import "./index.css";
+import App from "./App";
+
+configureProductsStore();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
